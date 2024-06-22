7fe0c3cdfcbdc07b1a9cfae64e334d2084fa423a
Chonakan Chumtap <cshuhand@gmail.com>
2024-06-22 23:27:09 +0700

    :chart_with_upwards_trend: : added visualization.

--- Linear-Regression/01.py
+++ Linear-Regression/01.py
@@ -1,29 +1,24 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn import datasets
 import matplotlib.pyplot as plt
-from LinearRegression import LinearRegression
+from model.LinearRegression import LinearRegression
+from plotter.contour import Plot_optimizationAndContour
 
-X_data = [[0], [2]]
-y_data = [0, 2]
+X_data = [[0], [2]] # X represents to be a row (samples)
+y_data = [0, 2] # Y represents to be a column (output)
 X = np.array(X_data)
 y = np.array(y_data)
 
-# print("X =", X, X.T)
-# print("y =", y)
+linear = LinearRegression(lr=0.1, n_iters=100) # start by learning rate sets to be 0.1 with number of 100 iterations (less data => high lr)
+linear.training(X, y)                          # optimize by using gradient descent
+predictions = linear.prediction(X)             # get the prediction output after optimization
+w_history, b_history = linear.get_Weigths_Bias_History() # get the value history for visualizing with contour
 
-reg = LinearRegression(lr=0.01, n_iters=1000)
-reg.training(X, y)
-predictions = reg.prediction(X)
-print(predictions)
+# mock the weigths (wi) and bias (w0) up from w_history and b_history to draw the contour
+weigths_range = np.linspace(-0.3, 1.5, 100)
+bias_range = np.linspace(-0.3, 1.5, 100)
 
-fig = plt.figure(figsize=(8,6))
-plt.scatter(X[:, 0], y, color = "b", marker = "o", s = 30)
-plt.plot(X, predictions, color='black', linewidth=2, label='Prediction')
-plt.show()
+# and pass them to calculate the costs
+W, B, cost_history = linear.generate_costs_forContour(X, y, weigths_range, bias_range)
 
-def costFunction(y, predictions):
-    return np.mean((y - predictions)**2) # Mean Sqaure Error (MSE)
-
-MSE = costFunction(y, predictions)
-print(MSE)
+# finally, plot them to visualization
+Plot_optimizationAndContour(predictions, X, y, w_history, b_history, W, B, cost_history)
---
+++ Linear-Regression/model/LinearRegression.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+def gradientDescent(n_samples, lr, X, y, y_pred):
+    d_weigths = (1 / n_samples) * np.dot(X.T, (y_pred - y)) # d_weights = 1 / n * ∑(y_prediction - y_actual) *  X
+    d_bias = (1 / n_samples) * np.sum(y_pred - y)           # d_bias = 1 / n * ∑(y_prediction - y_actual)
+    
+    weights_gradient = lr * d_weigths
+    bias_gradient = lr * d_bias
+    
+    return weights_gradient, bias_gradient
+
+def costFunction(n_samples, y_pred, y):     # Mean Sqaure Error (MSE)
+    return (1 / (2 * n_samples)) * np.sum((y_pred - y)**2)
+
+class LinearRegression:
+
+    def __init__(self, lr = 0.001, n_iters = 0):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weights = 0 # wi
+        self.bias = 0 # w0
+        self.n_samples = 0
+        self.weights_history = []
+        self.bias_history = []
+     
+    # Evaluation + Optimization   
+    def training(self, X, y):
+        n_samples, n_features = X.shape     # in the shape of n x m, which means n is number of samples and m means number of features
+        self.n_samples = n_samples          # set default number of samples
+        self.weights = np.zeros(n_features) # wights will be zero for all feature at the first time and the size of array depends on the total of features
+        
+        for _ in range(self.n_iters):
+            y_pred = self.prediction(X)
+            weights_gradient, bias_gradient = gradientDescent(n_samples, self.lr, X, y, y_pred)
+            self.weights -= weights_gradient
+            self.bias -= bias_gradient
+            self.weights_history.append(self.weights[0])
+            self.bias_history.append(self.bias)
+    
+    def generate_costs_forContour(self, X, y, w_range, b_range):
+        W, B = np.meshgrid(w_range, b_range)
+        costs_history = np.zeros(W.shape)
+        
+        for i in range(len(w_range)):
+            for j in range(len(b_range)):
+                weights = w_range[i]
+                bias = b_range[j]
+                y_pred = weights * X[:, 0] + bias
+                costs_history[j, i] = costFunction(self.n_samples, y_pred, y)
+                
+        return W, B, costs_history
+    
+    def get_Weigths_Bias_History(self):
+        return self.weights_history, self.bias_history
+
+    # Representation
+    def prediction(self, X):
+        # y = wx + b, in the form of matrix calculation y = (w * X) + b
+        return np.dot(X, self.weights) + self.bias
