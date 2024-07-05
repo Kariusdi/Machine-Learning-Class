7b65c42aeb25e5bf07c24ce7cfcc959808f6b9d2
[Sirirwat] <siriwat26room1@gmail.com>
2024-07-05 21:27:44 +0700

    .

--- Performance-Estimation/LabCode/LabCode.py
+++ Performance-Estimation/LabCode/LabCode.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import numpy as np
 
-#Data sampling
+#Data sampling#
 def Rand_DF(df ,rng_seed):
     rng_df = df.sample(n=1000, random_state = rng_seed)
     return rng_df
