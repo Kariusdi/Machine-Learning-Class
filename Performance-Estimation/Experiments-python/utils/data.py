2aae44e0be9198c5112cdcd54a6f626c41d94159
sariszoom <sariszoom@gmail.com>
2024-07-06 19:35:55 +0700

    comment code Lab1-4

--- Performance-Estimation/Experiments-python/utils/data.py
+++ Performance-Estimation/Experiments-python/utils/data.py
@@ -1,16 +1,21 @@
 # Data sampling
 def random_data(df ,seed, sample_size):
+    # สุ่มแถวจาก DataFrame ตามจำนวนที่ระบุใน sample_size โดยใช้ค่า seed
     rng_df = df.sample(n=sample_size, random_state=seed)
     return rng_df
 
 # For sampling observe
 def Test_output_handel(df):
     i = 0
+
+    # ทำการสุ่มข้อมูล 3 ครั้งโดยเปลี่ยนค่า seed ทีละ 1
     for i in range(3):
         random_data(df, 1+i).to_csv('rngdata{}.csv'.format(i+1), index= False) 
         print(i)
-        
+
+# ฟังก์ชันสำหรับเตรียมข้อมูล
 def Initialize_Data(df, Y_col):
+    # แยก features (X) และ target (y) จาก DataFrame
     X = df.drop(columns=[Y_col])
     y = df[Y_col]
     return X, y
--- Performance-Estimation/Experiments-python/utils/methods.py
+++ Performance-Estimation/Experiments-python/utils/methods.py
@@ -7,28 +7,35 @@ import numpy as np
 from experiments import Initialize_Data
 
 def HoldOut(df, Y_col, testsize):
+    # แยกข้อมูลเป็น X (feature) และ y (target) โดยใช้ฟังก์ชัน Initialize_Data
     X, y = Initialize_Data(df, Y_col)
-
+    # แบ่งข้อมูลเป็น train และ test set
     X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=testsize, random_state=0)
 
+    # สร้างและฝึกโมเดล Linear Regression
     model = LinearRegression()
     model.fit(X_train, Y_train)
 
+    # ทำนายค่า y จากข้อมูลทดสอบ
     y_pred = model.predict(X_test)
 
+    # คำนวณค่า RMSE
     RMSE = root_mean_squared_error(Y_test, y_pred)
     return RMSE
 
 def CrossValidation(df, Y_col, fold):
     X, y = Initialize_Data(df, Y_col)
 
+    # สร้าง KFold object สำหรับการทำ Cross-Validation
     kf = KFold(n_splits=fold, shuffle = True, random_state=0)
     
     rmse_values = []
+    # แบ่งข้อมูลเป็น k-folds และทำการฝึกโมเดลและทดสอบ
     for train_index, test_index in kf.split(X):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
+        # สร้างและฝึกโมเดล Linear Regression
         model = LinearRegression()
         model.fit(X_train, y_train)
 
