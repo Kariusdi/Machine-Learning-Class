6acb692f11fd48e51b1ea5dda427d1410999db18
sirapaop <sirapaop2545@gmail.com>
2024-08-02 17:03:47 +0700

    chang value

--- Linear Model Selection and Regularization/Ridge_ass1.py
+++ Linear Model Selection and Regularization/Ridge_ass1.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Set seed for reproducibility
+
 np.random.seed(0)
 X = np.random.rand(100, 1) * 10
 y = 2 * X.squeeze() + 1 + np.random.randn(100) * 2
 
-# Ridge Regression function
+
 def ridge_regression(X, y, alpha):
     n = len(X)
     mean_X = sum(X) / n
@@ -17,16 +17,16 @@ def ridge_regression(X, y, alpha):
     denominator = sum((X[i] - mean_X) ** 2 for i in range(n)) + alpha
     b1 = numerator / denominator
 
-    # intercept (b0)
+    # bias (b0)
     b0 = mean_y - b1 * mean_X
 
     return b0, b1
 
-# Prediction function
+
 def predict(X, b0, b1):
     return [b0 + b1 * x for x in X]
 
-# SSR + λ * slope^2 function
+# SSR + λ * slope^2 
 def ssr_plus_reg(X, y, slope, alpha):
     n = len(X)
     predicted_y = [slope * x + 1 for x in X]
@@ -35,7 +35,7 @@ def ssr_plus_reg(X, y, slope, alpha):
     alpha_slop2 = alpha * slope ** 2
     return squared_residuals + alpha_slop2
 
-# Plotting Data
+
 plt.figure(figsize=(14, 10))
 plt.subplot(2, 3, 1)
 plt.scatter(X, y, color='blue', edgecolor='k')
