d7e9292a556ee5f29504cf64264799556bfa87f5
[Sirirwat] <siriwat26room1@gmail.com>
2024-09-08 16:37:21 +0700

    .

--- Bayes/Quadratic.py
+++ Bayes/Quadratic.py
@@ -26,18 +26,33 @@ def qda_discriminant(x, mean, cov, prior):
     return -0.5 * np.dot(np.dot((x - mean).T, inv_cov), (x - mean)) - 0.5 * log_det_cov + np.log(prior)
 
 def qda_predict(X):
+    discriminants = Discriminants(X)
+    return np.argmax(discriminants, axis=1)
+
+def Discriminants(X):
     discriminants = np.array([
         [qda_discriminant(x, means[k], covariances[k], priors[k]) for k in range(len(means))]
         for x in X
     ])
-    return np.argmax(discriminants, axis=1)
+    return discriminants
+
+def qda_posterior(X):
+    discriminants = Discriminants(X)
+    max_discriminants = np.max(discriminants, axis=1, keepdims=True)
+    exp_discriminants = np.exp(discriminants - max_discriminants)
+    posteriors = exp_discriminants / np.sum(exp_discriminants, axis=1, keepdims=True)
+    return posteriors
+
 
 # Create a grid of points
 xx, yy = np.meshgrid(x_range,y_range)
 grid = np.c_[xx.ravel(), yy.ravel()]
 Z = qda_predict(grid).reshape(xx.shape)
 
+posterior_feature1 = qda_posterior(np.c_[x_range, np.zeros_like(x_range)])
+posterior_feature2 = qda_posterior(np.c_[np.zeros_like(y_range), y_range])
 
+#PLOTING LIKELIHOOD
 # Plot the likelihoods for feature 1
 plt.figure(figsize=(14, 6))
 plt.subplot(1, 2, 1)
@@ -55,9 +70,29 @@ plt.title('Likelihood for Feature 2')
 plt.xlabel('Feature 2')
 plt.ylabel('Probability Density')
 plt.legend()
+plt.tight_layout()
+plt.show()
+
+#PLOT A POSTERIOR
+fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+for i in range(2):
+    ax[0].plot(x_range, posterior_feature1[:, i], label=f'Class {i}')
+ax[0].set_title('Posterior Probability for Feature 1')
+ax[0].set_xlabel('Feature 1')
+ax[0].set_ylabel('Posterior Probability')
+ax[0].legend()
+
+# Feature 2
+for i in range(2):
+    ax[1].plot(y_range, posterior_feature2[:, i], label=f'Class {i}')
+ax[1].set_title('Posterior Probability for Feature 2')
+ax[1].set_xlabel('Feature 2')
+ax[1].set_ylabel('Posterior Probability')
+ax[1].legend()
 
 plt.tight_layout()
 plt.show()
+
 
 # PLOTING DECISION
 plt.figure(figsize=(10, 8))
